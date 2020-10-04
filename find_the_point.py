3aa0e0daea0e818ee5826bbbd632bc31e5abd113
Azad Kshitij <azadkshitij08302001@gmail.com>
2020-10-05 00:09:02 +0530

    commit

--- find_the_point.py
+++ find_the_point.py
@@ -1,12 +1,12 @@
 n = int(input())
 l = []
 for i in range(n):
-    px,py,qx,qy = map(int,input().split())
+    px, py, qx, qy = map(int, input().split())
     rx = (2*qx) - px
     ry = (2*qy) - py
     l.append(rx)
     l.append(ry)
-for i in range(0,2*n,2):
+for i in range(0, 2*n, 2):
     A = l[i]
     B = l[i+1]
-    print(A,B)
+    print(A, B)
